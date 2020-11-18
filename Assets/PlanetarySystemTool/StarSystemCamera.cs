0138ac13871b5fcd9fb200caa5cc1114657afff3
Antoine Pavy <antoine.pavy92@gmail.com>
2020-11-18 23:12:45 +0100

    new camera stationary mode

---
+++ Assets/PlanetarySystemTool/StarSystemCamera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using PlanetarySystemTool;
+using UnityEngine;
+
+public class StarSystemCamera : MonoBehaviour
+{
+    public enum CameraState {stationary,follow,look}
+
+    public CameraState cameraState;
+    public StarSystem system;
+    private Vector3 stationaryPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        stationaryPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        switch (cameraState)
+        {
+            case CameraState.stationary:
+                transform.LookAt(system.star);
+                break;
+            case CameraState.follow:
+                break;
+            case CameraState.look:
+                break;
+        }
+    }
+}
