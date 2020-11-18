5b0c5eaaa8d1f6deb8d78ca842867b008925bf43
Antoine Pavy <antoine.pavy92@gmail.com>
2020-11-18 23:26:09 +0100

    camera modes done

--- Assets/PlanetarySystemTool/StarSystemCamera.cs
+++ Assets/PlanetarySystemTool/StarSystemCamera.cs
@@ -6,8 +6,10 @@ using UnityEngine;
 public class StarSystemCamera : MonoBehaviour
 {
     public enum CameraState {stationary,follow,look}
-
+    
     public CameraState cameraState;
+    public int planetId;
+    public Vector3 followOffset;
     public StarSystem system;
     private Vector3 stationaryPosition;
     // Start is called before the first frame update
@@ -19,14 +21,25 @@ public class StarSystemCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(planetId >= system.planetsInfo.Count)
+        {
+            planetId = system.planetsInfo.Count - 1;
+        }else if(planetId < 0)
+        {
+            planetId = 0;
+        }
         switch (cameraState)
         {
             case CameraState.stationary:
+                transform.position = stationaryPosition;
                 transform.LookAt(system.star);
                 break;
             case CameraState.follow:
+                transform.position = followOffset + system.planetsInfo[planetId].planet.position;
+                transform.LookAt(system.planetsInfo[planetId].planet);
                 break;
             case CameraState.look:
+                transform.LookAt(system.planetsInfo[planetId].planet);
                 break;
         }
     }
